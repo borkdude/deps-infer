8869dbf43ae690c168b59e80240b1aefde76b1c9
Michiel Borkent <michielborkent@gmail.com>
2021-02-24 20:45:50 +0100

    Refactor

--- src/deps_infer/main.clj
+++ src/deps_infer/main.clj
@@ -4,42 +4,11 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.pprint :as pp]
-            [clojure.string :as str]
             [clojure.tools.cli :as cli]
+            [deps-infer.clojars :refer [index-jar lang]]
             [version-clj.core :as v]))
 
 (def suffix-re #"\.clj$|\.cljs$|\.cljc$") ;; Not including __init\.class$ as this gives false positives!
-
-(defn lang [filename]
-  (keyword (last (str/split filename #"\."))))
-
-(defn index-jar [index jar]
-  (let [jar (str jar)
-        jar-file (fs/file jar)]
-    (if-not (fs/directory? jar-file)
-      (with-open [jar-resource (java.util.jar.JarFile. jar-file)]
-        (let [entries (enumeration-seq (.entries jar-resource))]
-          (reduce (fn [acc e]
-                    (let [raw-name (.getName e)]
-                      (if (and (not (str/starts-with? raw-name "META"))
-                               (re-find suffix-re raw-name))
-                        (let [n (str/replace raw-name suffix-re "")
-                              n (str/replace n "_" "-")
-                              n (str/replace n "/" ".")
-                              n (symbol n)
-                              [_ group-id artifact version _]
-                              (re-find #"repository/(.*)/(.*)/(.*)/.*jar" jar)]
-                          (update acc n
-                                  (fn [deps]
-                                    (update deps (symbol (str/replace group-id "/" ".") artifact)
-                                            (fn [dep-info]
-                                              (-> dep-info
-                                                  (update :mvn/versions (fnil conj []) version)
-                                                  (update :langs (fnil conj #{}) (lang raw-name))))))))
-                        acc)))
-                  index
-                  entries)))
-      index)))
 
 (defn newest [versions snapshots]
   (reduce (fn [acc v]
@@ -54,18 +23,12 @@
                   v acc))))
           versions))
 
-(defn select-deps [source-lang deps]
+(defn select-deps [source-lang dep-entries]
   (case source-lang
-    (:cljs :cljc) deps
-    (reduce (fn [acc [k v]]
-              (let [langs (:langs v)
-                    match? (or (contains? langs :clj)
-                               (contains? langs :cljc))]
-                (if match?
-                  (assoc acc k v)
-                  acc)))
-            {}
-            deps)))
+    (:cljs :cljc) dep-entries
+    (filter (fn [entry]
+              (not= :cljs (lang (:file entry))))
+            dep-entries)))
 
 (def cli-options
   ;; An option with a required argument
@@ -96,20 +59,24 @@
             analysis (:analysis (clj-kondo/run! {:lint [(:analyze opts)]
                                                  :config {:output {:analysis true}}}))
             defined-namespaces (set (map :name (:namespace-definitions analysis)))
-            used-namespaces (distinct (map (juxt :to (comp lang :filename)) (:namespace-usages analysis)))
-            deps-map (reduce (fn [acc [n lang]]
-                               (if-let [deps-map (get index n)]
-                                 (let [deps-map (select-deps lang deps-map)]
-                                   (merge acc deps-map))
-                                 (do
-                                   (when-not (contains? defined-namespaces n)
-                                     (binding [*out* *err*]
-                                       (println "WARNING: no dep found for" n)))
-                                   acc))) {} used-namespaces)
+            used-namespaces (distinct (map (juxt :to (comp lang :filename))
+                                           (:namespace-usages analysis)))
+            entries (reduce (fn [acc [n lang]]
+                              (if-let [dep-entries (get index n)]
+                                (let [dep-entries (select-deps lang dep-entries)]
+                                  (into acc dep-entries))
+                                (do
+                                  (when-not (contains? defined-namespaces n)
+                                    (binding [*out* *err*]
+                                      (println "WARNING: no dep found for" n)))
+                                  acc)))
+                            [] used-namespaces)
+            grouped (group-by (juxt :group-id :artifact) entries)
             results (reduce (fn [acc [k v]]
-                              (assoc acc k {:mvn/version (newest (:mvn/versions v)
-                                                                 (:snapshots opts))}))
+                              (assoc acc (symbol (first k) (second k))
+                                     {:mvn/version (newest (map :mvn/version v)
+                                                           (:snapshots opts))}))
                             (sorted-map)
-                            deps-map)]
+                            grouped)]
         (doseq [[k v] results]
           (prn k v))))))
